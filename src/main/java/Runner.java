d2f15d06aac99052a4ffa7d2a34619e2423f97ba
nesibe <tnesibe.techproed@gmail.com>
2025-01-04 23:24:08 +0300

    goodbye

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,5 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Herkese iyi çalışmalar...");
         System.out.println("Başarılar...");
+        System.out.println("Advanced github dersinde görüşürüz:)");
     }
 }
